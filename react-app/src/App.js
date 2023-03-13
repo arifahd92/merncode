bc124876c060e7567b47fd48de39736f298af266
md arif <arifahd92@gmail.com>
2023-03-13 22:04:32 +0530

    khatabook

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
 
-const localizer = momentLocalizer(moment);
+import React from 'react'
+import Khatabook from './component/Khatabook'
 
-const App = () => {
-  const [appointments, setAppointments] = useState([]);
-
-  useEffect(() => {
-    // Fetch appointments from the server
-    async function fetchAppointments() {
-      const response = await fetch("/api/appointments");
-      const data = await response.json();
-      setAppointments(data);
-    }
-    fetchAppointments();
-  }, []);
-
-  const handleSelectEvent = event => {
-    // Handle appointment selection
-  };
-
+export default function App() {
   return (
-    <Calendar
-      localizer={localizer}
-      events={appointments}
-      onSelectEvent={handleSelectEvent}
-    />
-  );
-};
-
-export default App;
-
+    <div>
+     <Khatabook/>
+    </div>
+  )
+}
